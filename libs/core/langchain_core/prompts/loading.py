c8250fb533536ed3577987be6b7275ecea0d7bbd
gzaltran <gzaltran@gmail.com>
2024-05-03 17:48:18 -0300

    Add SimpleJsonOutputParser

--- libs/core/langchain_core/prompts/loading.py
+++ libs/core/langchain_core/prompts/loading.py
@@ -8,6 +8,7 @@ from typing import Callable, Dict, Union
 import yaml
 
 from langchain_core.output_parsers.string import StrOutputParser
+from langchain_core.output_parsers.json import SimpleJsonOutputParser
 from langchain_core.prompts.base import BasePromptTemplate
 from langchain_core.prompts.chat import ChatPromptTemplate
 from langchain_core.prompts.few_shot import FewShotPromptTemplate
@@ -79,6 +80,8 @@ def _load_output_parser(config: dict) -> dict:
         output_parser_type = _config.pop("_type")
         if output_parser_type == "default":
             output_parser = StrOutputParser(**_config)
+        elif output_parser_type == "simple_json_output_parser":
+            output_parser = SimpleJsonOutputParser(**_config)
         else:
             raise ValueError(f"Unsupported output parser {output_parser_type}")
         config["output_parser"] = output_parser
@@ -160,13 +163,8 @@ def _load_chat_prompt(config: Dict) -> ChatPromptTemplate:
     """Load chat prompt from config"""
 
     messages = config.pop("messages")
-    template = messages[0]["prompt"].pop("template") if messages else None
-    config.pop("input_variables")
-
-    if not template:
-        raise ValueError("Can't load chat prompt without template")
-
-    return ChatPromptTemplate.from_template(template=template, **config)
+    messages = [(m['type'], m['prompt']['template']) for m in messages]
+    return ChatPromptTemplate.from_messages(messages=messages)
 
 
 type_to_loader_dict: Dict[str, Callable[[dict], BasePromptTemplate]] = {
